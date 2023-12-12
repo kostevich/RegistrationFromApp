7330012222e535e5d8f2e3348678a34b26c682e5
Irisha <irisha.kostevich@mail.ru>
2023-12-12 20:07:37 +0300

    Refactoring

--- README.md
+++ README.md
@@ -1,32 +1,29 @@
-# RegistrationFromApp
+# RegistrationFromApp 
+**RegistrationFromApp** – приложение на [pqt6](https://github.com/piotrkowalczuk/pqt?ysclid=lq2khsyn8x955167959), которое вводит случайное, корректное имя пользователя, email, и номер телефона.
 
-Приложение, которое вводит случайное, корректное имя пользователя, email, и номер телефона.
-
-____
-## Порядок установки и использования
-1. Установить Python версии не старше [3.11](https://www.python.org/downloads/).
-
-2. В среду исполнения инсталлировать следующие пакеты : _pyqt6_, _Faker_, _selenium_, _pyinstaller_.
-
-`pip install pyqt6`
-
-`pip install Faker`
-
-`pip install selenium`
-
-`pip install pyinstaller`
-
+# Порядок установки и использования
+1. Загрузить репозиторий. Распаковать.
+2. Установить [Python](https://www.python.org/downloads/) версии не старше 3.11. Рекомендуется добавить в PATH.
+3. В среду исполнения установить следующие пакеты: [dublib](https://github.com/DUB1401/dublib), [Faker](https://github.com/faker-js/faker?ysclid=lq2kldnkwh96315556), [pyinstaller](https://github.com/pyinstaller?ysclid=lq2kngqhkw347498835), [pqt6](https://github.com/piotrkowalczuk/pqt?ysclid=lq2khsyn8x955167959), [selenium](https://github.com/SeleniumHQ/selenium?ysclid=lq2kp84y1834877858).
+```
+pip install Faker
+pip install git+https://github.com/DUB1401/dublib
+pip install pyinstaller
+pip install pyqt6
+pip install selenium
+```
 Либо установить сразу все пакеты при помощи следующей команды, выполненной из директории скрипта.
+```
+pip install -r requirements.txt
+```
+3. Открыть dist\main.exe.
+4. В поле ввода приложения вставить http://over.org.tilda.ws/testuser.
 
-`pip install -r requirements.txt`
+# Пример работы
+**Автоматическое заполнение информации на сайте:**
 
-3. Запустить main.py при помощи командной строки или интерпретатора Python.
+![image](https://github.com/kostevich/RegistrationFromApp/assets/109979502/96bd3846-bdf4-4251-9e27-9622b04ff003)
 
-4. В поле ввода приложения [вставить ссылку](http://over.org.tilda.ws/testuser).
-
-____
-## Пример работы
-
-![Пример работы](image.png)
+_Copyright © Kostevich Irina. 2023._
 
 
--- main.py
+++ main.py
@@ -1,68 +1,108 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout
-from PyQt6.QtCore import Qt
+ #!/usr/bin/python
+
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
+
 from PyQt6 import QtGui
-from selenium.webdriver.common.by import By
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLineEdit, QPushButton, QVBoxLayout, QWidget
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
 from Sourсe.RandomUserData import *
 from time import sleep 
 
 
 import sys
 
+#==========================================================================================#
+# >>>>> СОЗДАНИЕ ПРИЛОЖЕНИЯ <<<<< #
+#==========================================================================================#
+
 # Создадим приложение наследующее функции класса QWidget.
 class Window(QWidget):
     # Конструктор приложения.
     def __init__(self):
         super().__init__()
+
         # Размер окна приложения.
         self.resize(400, 250)
+
         # Название приложения.
         self.setWindowTitle("GeNERation DaTa")
+
         # Фон приложения.
         layout = QVBoxLayout()
         self.setLayout(layout)
+
         # Поле ввода текста.
         self.input = QLineEdit()
+
         # Размер поля.
         self.input.setFixedWidth(250)
+
         # Положение поля.
         layout.addWidget(self.input, alignment= Qt.AlignmentFlag.AlignCenter)
+
         # Создание кнопки.
         button = QPushButton("Generate")
+
         # Переход по ссылке при нажатии кнопки.
         button.clicked.connect(self.EnterUser)
+
         # Создание кнопки.
         layout.addWidget(button)
+
+#==========================================================================================#
+# >>>>> ВВОД ДАННЫХ НА САЙТЕ <<<<< #
+#==========================================================================================#
 
     # Ввод данных пользователя на сайте.
     def EnterUser(self):
         # Открытие браузера.
         driver = webdriver.Edge()
+
         # Открытие ссылки.
         driver.get(self.input.text())
+
         # Пока не закрыли окошко браузера.
         while True:
             # Формирование данных пользователя.
             username, email, number =  GenerateUser()
+
             # Ввод данных пользователя в нужные поля.
             driver.find_element(By.NAME,"Name").send_keys(username) 
             driver.find_element(By.NAME,"Email").send_keys(email) 
             driver.find_element(By.NAME,"tildaspec-phone-part[]").send_keys(number)
+
             # Нажатие кнопки. 
             driver.find_element(By.CLASS_NAME, "t-submit").click()
+
             # Ожидание 20 секунд.
             sleep(20)
+
             # Обновление страницы.
             driver.refresh() 
         
+#==========================================================================================#
+# >>>>> ИНИЦИАЛИЗАЦИЯ СКРИПТА <<<<< #
+#==========================================================================================#
+
 # Создаем экземпляр класса.
 app = QApplication(sys.argv)
+
 # Иконка приложения.
 app.setWindowIcon(QtGui.QIcon("auth.ico"))
+
 # Создаем виджет.
 window = Window()
 # Открытие окна приложения.
 window.show()
+
+#==========================================================================================#
+# >>>>> вЫХОД ИЗ ПРИЛОЖЕНИЯ <<<<< #
+#==========================================================================================#
+
 # Выход из приложения.
 sys.exit(app.exec())
 
